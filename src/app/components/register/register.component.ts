802f4979d882850c06dd1d0b5e1127e3a7d425bd
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-04-06 20:05:38 -0500

    Se utilizo el ngModel y se importo el modulo FormModule en app.module.ts

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from '../../models/user.model';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  user: UserModel;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.user = new UserModel();
+
+    this.user.email = 'jonan-vargas23@hotmail.com';
+  }
+
+  onSubmit() {
+    console.log("formulario enviado");
+    console.log(this.user);
   }
 
 }
