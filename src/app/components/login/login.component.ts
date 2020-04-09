c94e0d44818f71adf696fae9eb1448625efff3da
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-04-09 16:26:36 -0500

    Se hace validacion para el formulario de login por template

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserModel } from '../../models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user: UserModel;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.user = new UserModel();
+  }
+
+  login(formLogin: NgForm) {
+    console.log(formLogin.value);
   }
 
 }
