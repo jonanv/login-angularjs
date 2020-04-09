d5f86fa322889c8ddc18ee2b2f06765e68f867bd
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-04-09 15:38:36 -0500

    Se hace validacion de mensajes en el template con formRegister

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -15,8 +15,6 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.user = new UserModel();
-
-    this.user.email = 'jonan-vargas23@hotmail.com';
   }
 
   onSubmit(formRegister: NgForm) {
